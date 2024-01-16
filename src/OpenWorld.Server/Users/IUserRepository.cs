4f778b9ab536555291fa9b1b225442f6fb72538b
Adam Swanson <adam.swanson.98@gmail.com>
2024-01-16 17:10:19 -0500

    Create UserRepository

---
+++ src/OpenWorld.Server/Users/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using OpenWorld.Server.Users.Models;
+
+namespace OpenWorld.Server.Users;
+public interface IUserRepository
+{
+    bool Create(string username, string hashedPassword);
+    bool Delete(string username);
+    (bool Found, User? user) Get(string username);
+}
--- src/OpenWorld.Server/Users/UserService.cs
+++ src/OpenWorld.Server/Users/UserService.cs
@@ -4,20 +4,17 @@ using OpenWorld.Server.Users.Models;
 namespace OpenWorld.Server.Users;
 
 public class UserService(
+    IUserRepository userRepository,
     IPasswordHashingService passwordHashingService,
     ILogger<UserService> logger) : IUserService
 {
+    private readonly IUserRepository _userRepository = userRepository;
     private readonly IPasswordHashingService _passwordHashingService = passwordHashingService;
     private readonly ILogger<UserService> _logger = logger;
 
-    private readonly Dictionary<string, User> _users = new()
-    {
-        { "admin", new User("admin", "79b890890374fcc41cd8643eeb8095f54b57607f271a43dcd6f43aaf4e445a164c47b116bde0b5f782b968cb15d734154da7eb0bac3fecc837cdf50f402b8a1c", "Administrator") }
-    };
-
     public User? GetUser(string username)
     {
-        var successfullyFound = _users.TryGetValue(username, out var user);
+        var (successfullyFound, user) = _userRepository.Get(username);
 
         if (!successfullyFound)
         {
@@ -33,14 +30,14 @@ public class UserService(
 
     public async Task AddUser(string username, string password)
     {
-        if (_users.ContainsKey(username))
+        if (_userRepository.Get(username).Found)
         {
             throw new Exception($"User already exists with username '{username}'.");
         }
 
         var hashedPassword = await _passwordHashingService.HashPasswordAsync(password);
 
-        var successfullyAdded = _users.TryAdd(username, new User(username, hashedPassword, "User"));
+        var successfullyAdded = _userRepository.Create(username, hashedPassword);
 
         if (successfullyAdded)
         {
@@ -54,12 +51,12 @@ public class UserService(
 
     public void RemoveUser(string username)
     {
-        if (!_users.ContainsKey(username))
+        if (!_userRepository.Get(username).Found)
         {
             throw new Exception($"User does not exist with username '{username}'.");
         }
 
-        var successfullyRemoved = _users.Remove(username);
+        var successfullyRemoved = _userRepository.Delete(username);
 
         if (successfullyRemoved)
         {
